2ff8dadc6d10f808a834bc591af42919d1e21b1a
Kunal <raikunal251297@gmail.com>
2022-03-22 19:02:34 +0530

    update: Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+       KUNAL
+        
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
